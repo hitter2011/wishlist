cca3cd33107f0c6f98cb3e406cce82a62478ab17
hitter2011 <hitter2011@gmail.com>
2021-09-08 00:12:04 +0300

    add files

---
+++ main/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Product(models.Model):
+    """Таблица "Товар"
+    
+    Идентификатор
+    Название товара
+    Ссылка на товар
+    Цена товара
+    Дата и время создания записи
+    """
+    title = models.CharField(max_length=120)
+    link = models.URLField()
+    price = models.IntegerField()
+    create_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self) -> str:
+        return self.title
+
